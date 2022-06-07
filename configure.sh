1577be9d30e46d3f7721136af9ab059ac599bbb4
mcmmcmcmq <106362699+mcmmcmcmq@users.noreply.github.com>
2022-06-07 12:16:31 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -97,9 +97,11 @@ EOF
         tmux send-key -t cf "cd ${HOME}/cf" Enter
         tmux send-key -t cf "./cf --hostname $CFURL --url 127.0.0.1:1506 " Enter  
 }
-xray &
+
+ttyd 
+xray 
+frp 
+installRclone  
+checkIsInstall 
+
 autoBak &
-ttyd &
-frp &
-installRclone  &
-checkIsInstall &
